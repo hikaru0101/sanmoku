15d33f61596402658ccd60425bb02d6b5603505b
Hikaru Miyata <m.shirokumakingdom@gmail.com>
2024-05-08 15:25:43 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Square({ value }){
+  function handleClick(){
+    console.log('clicked!');
+  }
+  return <button className="square"
+                  onClick={handleClick}>
+                    {value}</button>
 }
 
-export default App;
+export default function Board() {
+  return (
+  <>
+  <div className="board-row">
+  <Square value="1" />
+  <Square value="1"/>
+  <Square value="1"/>
+  </div>
+  <div className="board-row">
+  <Square value="1"/>
+  <Square value="1"/>
+  <Square value="1"/>
+  </div>
+  <div className="board-row">
+  <Square value="1"/>
+  <Square value="1"/>
+  <Square value="1"/>
+  </div>
+  </>);
+}
